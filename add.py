cd6e7f7e784921fa534c802c33f3be89931f654e
Muzammil Shaikh <Skullbreaker722@gmail.com>
2025-03-05 13:23:32 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,19 @@
+# age=14
+# if age>=18 :
+#    if age<50 :
+#       print("Age is less than 50")
+#    else :
+#       print("Age is greater than or equal to 50")
+# else :
+#     if age>=13 :
+#        print("Teenager")
+#     else :
+#        print("Child")
+
+
+def add(a,b):
+   sum = a+b
+   print("SUM:",sum)
+
+
+add(10,20)
